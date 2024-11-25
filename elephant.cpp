6f66282367d32c11776795a1ec17ddda32b2dc52
manoshi14 <farzanazahid314@gmail.com>
2024-11-25 18:58:25 +0600

    Add files via upload

---
+++ elephant.cpp
@@ -0,0 +1,24 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int x;
+
+{
+ cin>>x;
+
+
+}
+if(x==1 || x==2 || x==3 || x==4 || x==5)
+    cout<<1<<endl;
+
+
+
+
+
+
+
+
+
+
+}
---
+++ translation.cpp
@@ -0,0 +1,34 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{string s,t;
+
+string i;
+cin>>s;
+reverse(s.begin(),s.end());
+i=s;
+cin>>t;
+if(i==t)
+cout<<"YES"<<endl;
+else
+    cout<<"NO"<<endl;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
