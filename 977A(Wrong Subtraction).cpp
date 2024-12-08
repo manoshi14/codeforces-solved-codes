42b28cb7cb3f5985fbe37097c89ff5c35b05c6af
manoshi14 <farzanazahid314@gmail.com>
2024-12-09 00:33:33 +0600

    Add files via upload

---
+++ 977A(Wrong Subtraction).cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ int main()
+ {int n,k,i;
+
+ cin>>n;
+ cin>>k;
+{
+
+
+ for(i=0;i<k;i++)
+ {
+ if(n%10!=0)
+ {
+     n=n-1;
+ }
+ else
+ {
+   n=n/10;
+ }
+ }
+ cout<<n<<endl;
+
+
+
+
+
+
+ }
+
+
+
+ }
+
