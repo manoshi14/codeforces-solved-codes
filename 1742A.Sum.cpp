67c6b3381470d8d8d23b5a90fb73a7bf9e6baf07
manoshi14 <farzanazahid314@gmail.com>
2024-12-09 23:48:28 +0600

    Add files via upload

---
+++ 1742A.Sum.cpp
@@ -0,0 +1,34 @@
+
+
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main()
+
+
+    {int a,b,c,i,t;
+    cin>>t;
+    while(t--)
+
+   {
+
+
+
+    cin>>a;
+    cin>>b;
+    cin>>c;
+
+
+
+        if(a+b==c || b+c==a || c+a==b )
+        cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+
+
+       }
+
+
+
+
+
+    }
