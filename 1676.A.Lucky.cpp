ef5a481975bc21b125d93bd5912cf26d1269f4f4
manoshi14 <farzanazahid314@gmail.com>
2024-12-10 14:56:40 +0600

    Add files via upload

---
+++ 1676.A.Lucky.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int t,i;
+   string s;
+   cin>>t;
+
+   while(t--)
+   {
+ cin>>s;
+
+if(s[0]+s[1]+s[2]==s[3]+s[4]+s[5])
+        cout<<"yes"<<endl;
+      else
+
+        cout<<"no"<<endl;
+
+
+
+
+
+
+
+
+
+}
+
+}
