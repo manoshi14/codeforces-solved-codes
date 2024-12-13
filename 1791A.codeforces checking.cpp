17df14450073046974d06882b8d4f0004eecddda
manoshi14 <farzanazahid314@gmail.com>
2024-12-14 01:56:18 +0600

    Add files via upload

---
+++ 1791A.codeforces checking.cpp
@@ -0,0 +1,25 @@
+
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main()
+
+
+    {string s="codeforces";
+    int i,t;
+cin>>t;
+while(t--){
+      char m;
+cin>>m;
+
+
+    {if(m=='c'||m=='o'||m=='d'||m=='e'||m=='f'||m=='r'||m=='s')
+        cout<<"yes"<<endl;
+        else
+            cout<<"no"<<endl;
+
+
+
+
+    }
+}
+    }
